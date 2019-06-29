72ceb5d00d7c2bd961f852d464e0d01bfa7e7746
Jitendra Purbey <jitendrapurbey@gmail.com>
2019-06-29 23:23:08 +0530

    added config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Twitter authentication keys
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+
+# Elasticsearch authentication keys
+ELASTIC_USERNAME=""
+ELASTIC_PASSWORD=""
